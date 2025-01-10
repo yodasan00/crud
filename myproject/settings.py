b0ce91e0626feb15a14368d541f5ce9b8af7e196
pushkar raj <pushkarraj192003@gmail.com>
2025-01-10 12:07:51 +0530

    postgres connected
    dbname: excise_db
    pass:192003
    
    added reqirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'excise_db',
+        'USER': 'postgres',
+        'PASSWORD':'192003',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
-}
+
+    }
+
 
 
 # Password validation
