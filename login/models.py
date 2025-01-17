3687b0d6e0dd7cd4e4acff82d318ed96d64ae95e
pushkar raj <pushkarraj192003@gmail.com>
2025-01-17 08:17:46 +0530

    commit

--- login/models.py
+++ login/models.py
@@ -18,7 +18,7 @@ validate_4_digit_otp = RegexValidator(
 class UserDetails(models.Model):
     user_profile = models.OneToOneField(User, on_delete=models.CASCADE ,related_name="user_details") 
     date_of_birth = models.DateField(default=None)
-    user_status = models.BooleanField(default=True)
+    user_status = models.BooleanField(default=False)
     phone_number = models.CharField(max_length=10, validators=[phone_number_validator],unique=True)
 
     def __str__(self): 
